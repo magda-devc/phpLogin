536237239d44655d3ce8c1540bf75cda72101c45
Magdalene <mgchebet@gmail.com>
2023-11-20 19:45:32 +0300

    trying to make changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (isset($_SESSION["useruid"])) {
 </div>
 <div class="container row">
     <div class="col-sm-6">
-        <p>Community funds is all about 'chama'. <br>
+        <p>Community funds is all about 'chama' and saving money. <br>
         Your contributions made this month to the charity basket is Ksh.2000. <br>
         Grow profits with your money by buying bonds in the community bank.</p>
     </div>
